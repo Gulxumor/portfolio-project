04dd93d88b4094ffb930b983216a9996849d065d
Gulxumor <gulxumorerkinjonova2005@gmail.com>
2023-12-28 09:39:55 +0500

    config file updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,30 +8,13 @@ module.exports = {
         lightOrange: "#fdba74",
         kulrang: "#f7f7ff",
         kulrang_text: "#7b7b7b",
-        white: "#ffffff",
         black: "#0b0b13",
       },
       fontFamily: {
         rubik: ["Rubik", "sans-serif", "monospac"],
         mario: ["New Super Mario Font U", "sans-serif"],
-        gagalin: ["Gagalin","sans-serif"],
+        gagalin: ["Gagalin", "sans-serif"],
       },
-    },
-    screens: {
-      sm: "640px",
-      // => @media (min-width: 640px) { ... }
-
-      md: "768px",
-      // => @media (min-width: 768px) { ... }
-
-      lg: "1024px",
-      // => @media (min-width: 1024px) { ... }
-
-      xl: "1280px",
-      // => @media (min-width: 1280px) { ... }
-
-      "2xl": "1536px",
-      // => @media (min-width: 1536px) { ... }
     },
   },
   plugins: [],
